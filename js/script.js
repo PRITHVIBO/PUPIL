ccf724f06f66f84f974f3c2a44d9e84ec3b37b06
Bhavya Gupta <bhavyag862@gmail.com>
2026-08-06 22:35:54 +0530

    Push few files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(function (button) {
+
+    button.addEventListener("click", function () {
+
+        alert("Congratulations! You have successfully enrolled.");
+
+    });
+
+});
